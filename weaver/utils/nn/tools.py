f1649426b02fe695f7a9c6f982f9437e2d7919c6
har-zia <harzia360@gmail.com>
2026-02-05 12:54:00 -0800

    Revert back tensorboard comment

--- weaver/utils/nn/tools.py
+++ weaver/utils/nn/tools.py
@@ -489,7 +489,7 @@ class TensorboardHelper(object):
     def __init__(self, tb_comment, tb_custom_fn):
         self.tb_comment = tb_comment
         from torch.utils.tensorboard import SummaryWriter
-        self.writer = SummaryWriter(log_dir=self.tb_comment)
+        self.writer = SummaryWriter(comment=self.tb_comment)
         _logger.info('Create Tensorboard summary writer with comment %s' % self.tb_comment)
 
         # initiate the batch state
